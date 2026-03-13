a710f25145309957bda2303b0f6adb4c0269bab5
Gabriel de la Cerda <81375591+Gabrieldelacerda@users.noreply.github.com>
2026-03-13 12:01:03 -0300

    Add files via upload

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "key_name" {
   description = "Name of your AWS EC2 Key Pair"
   type        = string
 }
+
+variable "my_ip" {
+  description = "IP address allowed to SSH into the instance"
+  type        = string
+}
